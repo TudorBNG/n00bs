a18e3ecba33d79ca812654a2638e475b8513a937
Arrriba <arina.cazacu@gmail.com>
2019-12-09 21:34:49 +0200

    added rating stars and some more rendering for game cards

--- frontend/src/components/GameCard.jsx
+++ frontend/src/components/GameCard.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import '../styles/components/GameCard.scss';
 import '../models/Game.ts';
+import StarRatings from 'react-star-ratings';
+import { number } from 'prop-types';
+
 
 class GameCard extends Component {
 
-
+  state = {
+    rating: number
+  };
 
   render() {
     const game = this.props.game;
@@ -16,13 +21,23 @@ class GameCard extends Component {
         </div>
 
         <div className="content-container">
-          <div>
-            {game.name}
-          </div>
-          <div>
-            {game.rating}
-          </div>
-          <div>
+          <span className="title-rating">
+            <div>
+              {game.name}
+            </div>
+            <div>
+              <StarRatings
+                rating={game.rating}
+                starRatedColor="#F86210"
+                changeRating={this.changeRating}
+                numberOfStars={5}
+                name='rating'
+                starDimension="15px"
+                starSpacing="2px"
+              />
+            </div>
+          </span>
+          <div className="description-container">
             {game.description}
           </div>
         </div>
